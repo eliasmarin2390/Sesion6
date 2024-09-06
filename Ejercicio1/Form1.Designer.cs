1ed8ef261245c02ac1de3f8a488bce9d56294a8b
elias <eamarin@uamv.edu.ni>
2024-09-06 16:01:47 -0600

    Elias realizo este programa

---
+++ Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbNombres = new System.Windows.Forms.ComboBox();
+            this.cmbnombres2 = new System.Windows.Forms.ComboBox();
+            this.btnmover = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(142, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.tbNombre.Location = new System.Drawing.Point(255, 62);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(238, 22);
+            this.tbNombre.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(522, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 77);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbNombres
+            // 
+            this.cmbNombres.FormattingEnabled = true;
+            this.cmbNombres.Location = new System.Drawing.Point(225, 125);
+            this.cmbNombres.Name = "cmbNombres";
+            this.cmbNombres.Size = new System.Drawing.Size(315, 24);
+            this.cmbNombres.TabIndex = 3;
+            // 
+            // cmbnombres2
+            // 
+            this.cmbnombres2.FormattingEnabled = true;
+            this.cmbnombres2.Location = new System.Drawing.Point(225, 246);
+            this.cmbnombres2.Name = "cmbnombres2";
+            this.cmbnombres2.Size = new System.Drawing.Size(315, 24);
+            this.cmbnombres2.TabIndex = 4;
+            // 
+            // btnmover
+            // 
+            this.btnmover.Location = new System.Drawing.Point(225, 178);
+            this.btnmover.Name = "btnmover";
+            this.btnmover.Size = new System.Drawing.Size(315, 34);
+            this.btnmover.TabIndex = 5;
+            this.btnmover.Text = "Mover pa donde quieras";
+            this.btnmover.UseVisualStyleBackColor = true;
+            this.btnmover.Click += new System.EventHandler(this.btnmover_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnmover);
+            this.Controls.Add(this.cmbnombres2);
+            this.Controls.Add(this.cmbNombres);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbNombres;
+        private System.Windows.Forms.ComboBox cmbnombres2;
+        private System.Windows.Forms.Button btnmover;
+    }
+}
+
